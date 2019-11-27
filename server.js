66f05fda95db5723b70ac75bf46c09291e31dc8e
Glitch (mylocation) <none>
2019-11-27 14:01:32 +0000

    🍜📫 4 finished

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.listen(3000, () => console.log("Listening at Port: 3000."));
 // Serve static files through express
 app.use(express.static("public"));
 
-// Detect device information through npm package called express-useragent
-const useragent = require('express-useragent');
-app.use(useragent.express());
-
 // 3. Parse incoming data as JSON and limit it to 1mb to avoid flooding
 app.use(express.json({limit:'1mb'}));
 
@@ -27,21 +23,22 @@ const database = new Datastore({ filename: '.data/database.db', autoload: true }
 //3 steps needed: Routing, JSON parsing, and GET request with fetch()
 // III.1. POST method route - What i am really doing is creating an API! Next i need to POST data from client to here (step 2 is in client code)
 app.post('/api', (request, response) => {
-  console.log('I got a request from: ' + request.useragent.source);//line 8,9 - You see this in server console
   console.log(request.body );
   console.log("--------------")
   //response.end(); to finish. or just send some data back like below
   //You see this in Client console :)
   
-  // IV.3 Push information into the DB.
+  // IV.3 Push data into the Database
   database.insert(request.body);
   
   response.json({
     status: "It worked!",
-    latitude: request.body.lat,
-    longitude: request.body.lon,
-    device: request.useragent.source,
-    dateTime: request.body.dateTime
+    //device: request.body.device,
+    //latitude: request.body.lat,
+    //longitude: request.body.lon,
+    //dateTime: request.body.dateTime,
+    //network: request.body.network,
+    body: request.body
   }); 
 });
 
